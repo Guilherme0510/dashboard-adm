adf995faf0e3d3e40357f1719ea24a995f8e1663
Guilherme <guissilval005@gmail.com>
2025-02-26 11:48:07 -0300

    bug: alterando o cors para o backend funcionar

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,19 @@
-import dotenv from 'dotenv' 
+import dotenv from 'dotenv';
 import express from 'express';
-import cors from 'cors' 
+import cors from 'cors';
 import { userRouter } from './routes/userRoutes.js';
+
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: 'https://dashboard-adm-front-end.vercel.app',  
+};
+
+app.use(cors(corsOptions)); 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/api', userRouter)
+app.use('/api', userRouter);
+
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
