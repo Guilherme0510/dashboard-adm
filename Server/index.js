75dc4c771e49b849cee2151314b55f8424f071ea
Guilherme <guissilval005@gmail.com>
2025-02-26 13:19:30 -0300

    bug: alterando o cors para o backend funcionar 4

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({
     origin: process.env.FRONTEND_URL || "https://dashboard-adm-front-end.vercel.app",
-    // origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    // origin: process.env.FRONTEND_URL || "http://localhost:5173",
   }));
 app.use(express.json());
 
